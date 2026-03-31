ad5027367238ef7daaee1a002b1f228a16acc04a
Matthias Rudingsdorfer <matthias.rudingsdorfer@planic.net>
2026-03-31 22:28:35 +0200

    chore: test cached pipeline performance

--- apps/hearthly-api/src/app/app.service.ts
+++ apps/hearthly-api/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello from Hearthly API' };
+    return { message: 'Hearthly API v0.3.0' };
   }
 }
