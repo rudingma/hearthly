4fc4181cd5138c0040fda38c087c7baa4c2b854b
Matthias Rudingsdorfer <matthias.rudingsdorfer@planic.net>
2026-03-31 22:37:59 +0200

    chore: clean up API message

--- apps/hearthly-api/src/app/app.service.ts
+++ apps/hearthly-api/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hearthly API v0.3.0' };
+    return { message: 'Hearthly API' };
   }
 }
