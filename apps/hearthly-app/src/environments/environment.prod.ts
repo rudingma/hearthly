c73b632562034af29b529b5564f59f994c388faf
Matthias Rudingsdorfer <matthias.rudingsdorfer@gmail.com>
2026-04-06 19:13:02 +0200

    feat(app): add environment configuration for OIDC and GraphQL (#9)

---
+++ apps/hearthly-app/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  keycloak: {
+    issuer: 'https://auth.hearthly.dev/realms/hearthly',
+    clientId: 'hearthly-app',
+  },
+  graphql: {
+    uri: 'https://api.hearthly.dev/graphql',
+  },
+};
