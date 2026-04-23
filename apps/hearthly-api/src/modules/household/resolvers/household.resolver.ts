d2dbf2b95c939dce963ed5d712f97e11bb9360c2
Matthias Rudingsdorfer <matthias.rudingsdorfer@gmail.com>
2026-04-23 19:00:00 +0200

    feat(api): HouseholdResolver for myHouseholds + createHousehold (#113)

---
+++ apps/hearthly-api/src/modules/household/resolvers/household.resolver.ts
@@ -0,0 +1,48 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Household } from '../models/household.model';
+import { CreateHouseholdInput } from '../inputs/create-household.input';
+import { CreateHouseholdPayload } from '../payloads/create-household.payload';
+import { HouseholdService } from '../household.service';
+import { CurrentUser, type JwtPayload } from '../../auth';
+import { UserService } from '../../user/user.service';
+
+function jwtToClaims(jwt: JwtPayload) {
+  return {
+    sub: jwt.sub,
+    email: jwt.email,
+    name: jwt.name,
+    picture: jwt.picture,
+  };
+}
+
+@Resolver(() => Household)
+export class HouseholdResolver {
+  constructor(
+    private readonly householdService: HouseholdService,
+    private readonly userService: UserService
+  ) {}
+
+  @Query(() => [Household], {
+    description: 'Returns households the current user is a member of',
+  })
+  async myHouseholds(@CurrentUser() jwt: JwtPayload): Promise<Household[]> {
+    const user = await this.userService.getOrSyncByKeycloakId(jwtToClaims(jwt));
+    return this.householdService.listForUser(user.id);
+  }
+
+  @Mutation(() => CreateHouseholdPayload)
+  async createHousehold(
+    @CurrentUser() jwt: JwtPayload,
+    @Args('input') input: CreateHouseholdInput
+  ): Promise<CreateHouseholdPayload> {
+    const user = await this.userService.getOrSyncByKeycloakId(jwtToClaims(jwt));
+    const household = await this.householdService.create(
+      user.id,
+      input.name,
+      input.clientMutationId
+    );
+    const payload = new CreateHouseholdPayload();
+    payload.household = household;
+    return payload;
+  }
+}
